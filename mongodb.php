757315718e6f0523aec710cfc70af7d5db986e2e
Ilya Krasilnikov <krasilnikov.ilya@gmail.com>
2009-11-09 02:41:02 +0300

    some fixes

--- mongodb.php
+++ mongodb.php
@@ -5,11 +5,14 @@ require 'Benchmark/Timer.php';
 $timer = new Benchmark_Timer();
 
 $mongo_cnn = new Mongo();
-$mongo = $mongo_cnn->selectDB( "php_db" )->selectCollection( "dmdb" );
 
 $points = array(1000, 10000, 100000, 500000, 1000000);
 
 foreach($points as $val){
+  $mongo_cnn->selectDB("php_db")->selectCollection("dmdb")->drop;
+  $mongo = $mongo_cnn->selectDB("php_db")->selectCollection("dmdb");
+  $mongo->insert(array('stub' => 'I am empty init doc'));
+
   $startmark = $val . ' start';
   $stopmark = $val . ' stop';
   
@@ -22,14 +25,12 @@ foreach($points as $val){
 
 function cycle($c){
   global $mongo;
-  
-  $mongo->remove();
-  
+
   for($i = 0; $i<$c; $i++){
     $doc = array(
       "text" => "i am any text",
       "count" => $i,
-      "coords" => (object)array( "x" => 100, "y" => 200, "z" => $i )
+      "coords" => (object)array("x" => 100, "y" => 200, "z" => $i)
     );
     
     $mongo->insert($doc);
